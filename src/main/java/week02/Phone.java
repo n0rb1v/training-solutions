66a29f372523dd8418c5a1e7b967aa5a5c0e9f28
n0rb1v <n0rb1v@gmail.com>
2020-11-03 10:18:45 +0100

    week02

---
+++ src/main/java/week02/Phone.java
@@ -0,0 +1,27 @@
+package week02;
+
+public class Phone {
+    String type;
+    int mem;
+
+    public Phone(String type, int mem) {
+        this.type = type;
+        this.mem = mem;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMem() {
+        return mem;
+    }
+
+    public void setMem(int mem) {
+        this.mem = mem;
+    }
+}
---
+++ src/main/java/week02/PhoneMain.java
@@ -0,0 +1,27 @@
+package week02;
+
+import java.util.Scanner;
+
+public class PhoneMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("tipus?");
+        String x=scanner.nextLine();
+        System.out.println("mem meret");
+        int i= scanner.nextInt();
+        Phone phone1=new Phone(x,i);
+        x=scanner.nextLine();
+
+
+        System.out.println("tipus?");
+        x=scanner.nextLine();
+        System.out.println("mem meret");
+        i= scanner.nextInt();
+        Phone phone2=new Phone(x,i);
+
+        System.out.println(phone1.getType()+" "+phone1.getMem());;
+        System.out.println(phone2.getType()+" "+phone2.getMem());;
+
+
+    }
+}

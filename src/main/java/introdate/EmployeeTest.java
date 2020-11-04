7d9d0b1b55f96b8d518a0e0ff829c226657bb427
n0rb1v <n0rb1v@gmail.com>
2020-11-04 20:59:12 +0100

    employee

---
+++ src/main/java/introdate/EmployeeTest.java
@@ -0,0 +1,9 @@
+package introdate;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee employee = new Employee(1992, 2, 2, "Kovács Béla");
+        System.out.println(employee.getBeginEmployment());
+        System.out.println(employee.getDateOfBirth());
+    }
+}

6c5954dd1dff5fa6e9e33f6deb4d88ba91ecba88
n0rb1v <n0rb1v@gmail.com>
2020-11-04 11:15:03 +0100

    product

---
+++ src/main/java/week02/Product.java
@@ -0,0 +1,33 @@
+package week02;
+
+public class Product {
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean theyEqual(Product p) {
+        return this.name.equals(p.getName());
+    }
+
+    public static void main(String[] args) {
+        Product product1 = new Product("konyv","0001");
+        Product product2 = new Product("konyv","0002");
+        Product product3 = new Product("ujsag","0003");
+
+        System.out.println(product1.theyEqual(product2));
+        System.out.println(product1.theyEqual(product3));
+
+    }
+}

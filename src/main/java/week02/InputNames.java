e5a590816dd82e389a1259d51ba42df7d6c467e7
n0rb1v <n0rb1v@gmail.com>
2020-11-05 10:28:05 +0100

    kereses

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,20 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] nevek = new String[5];
+
+        for (int i=0;i<nevek.length;i++){
+            System.out.println("neve?");
+            nevek[i]=scanner.nextLine();
+        }
+        for (int i=0;i<nevek.length;i++){
+            System.out.println(nevek[i]);
+        }
+
+
+    }
+}

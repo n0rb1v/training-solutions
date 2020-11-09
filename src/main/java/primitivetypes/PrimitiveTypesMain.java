ab3c05e05381b0c03bd0ea434cfbad1e6b17b640
n0rb1v <n0rb1v@gmail.com>
2020-11-09 19:49:54 +0100

    kettes szamrendszer

---
+++ src/main/java/primitivetypes/PrimitiveTypesMain.java
@@ -0,0 +1,8 @@
+package primitivetypes;
+
+public class PrimitiveTypesMain {
+    public static void main(String[] args) {
+        System.out.println(new PrimitiveTypes().tobinaryString(-100));
+        System.out.println(Integer.toBinaryString(-100));
+    }
+}

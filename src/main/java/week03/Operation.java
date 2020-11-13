8819bad687f6213ba9aa0f289a7093c20ed1a333
n0rb1v <n0rb1v@gmail.com>
2020-11-13 10:54:43 +0100

    operation

--- src/main/java/week03/Operation.java
+++ src/main/java/week03/Operation.java
@@ -5,7 +5,7 @@ public class Operation {
     private int rightOp;
 
     public  Operation(String s) {
-        leftOp = Integer.parseInt(s.substring(0,s.indexOf("+")-1));
+        leftOp = Integer.parseInt(s.substring(0,s.indexOf("+")));
         rightOp = Integer.parseInt(s.substring(s.indexOf("+"),s.length()));
     }
 

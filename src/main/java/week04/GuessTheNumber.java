10d2b917643b8e5c9399e205c212fe622754fc94
n0rb1v <n0rb1v@gmail.com>
2020-11-18 10:40:03 +0100

    guess

---
+++ src/main/java/week04/GuessTheNumber.java
@@ -0,0 +1,33 @@
+package week04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+    public static void main(String[] args) {
+        final Random rnd = new Random();
+
+        int number = rnd.nextInt(99)+1;
+        int guess = 0;
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Szam?");
+            guess = Integer.parseInt(scanner.nextLine());
+            if (guess < number){
+                System.out.println("kisebb");
+            }
+            if (guess > number){
+                System.out.println("nagyobb");
+            }
+            if (guess == number){
+                System.out.println("Kitalatad "+number );
+            }
+
+        }
+        System.out.println("Elfogyott a lehetoseg");
+
+    }
+}

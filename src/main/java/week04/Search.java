d9fa35f6afd6e6f29c1165187f2de7737e3f31dc
n0rb1v <n0rb1v@gmail.com>
2020-11-20 15:57:49 +0100

    week04

---
+++ src/main/java/week04/Search.java
@@ -0,0 +1,17 @@
+package week04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search {
+    public List<Integer> getIndexesOfChar(String str, char c) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+}

9a12573cd2bcdae6116173118a32124ea2e6d125
n0rb1v <n0rb1v@gmail.com>
2020-12-06 19:17:30 +0100

    w6d5

--- src/main/java/week06d05/Biscuit.java
+++ src/main/java/week06d05/Biscuit.java
@@ -1,5 +1,27 @@
 package week06d05;
 
 public class Biscuit {
-    private
+    private BiscuitType type;
+    private int gramAmount;
+
+    public Biscuit(BiscuitType type, int gramAmount) {
+        this.type = type;
+        this.gramAmount = gramAmount;
+    }
+
+    public static Biscuit of(BiscuitType bis, int gram){
+        return new Biscuit(bis,gram);
+    }
+
+    @Override
+    public String toString() {
+        return "Biscuit{" +
+                "type=" + type +
+                ", gramAmount=" + gramAmount +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Biscuit.of(BiscuitType.CSOKIS,20));
+    }
 }

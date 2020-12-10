52eb956bbfb940bf74161f031ac23569f09336f0
n0rb1v <n0rb1v@gmail.com>
2020-12-10 11:09:29 +0100

    w7d3

---
+++ src/main/java/week07d03/NumberList.java
@@ -0,0 +1,17 @@
+package week07d03;
+
+import java.util.List;
+
+public class NumberList {
+    public static boolean isIncreasing(List<Integer> numbers) {
+        int prev = 0;
+        for (Integer item : numbers) {
+            if (item >= prev) {
+                prev=item;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/week07d03/NumberListTest.java
@@ -0,0 +1,17 @@
+package week07d03;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumberListTest {
+    @Test
+    public void test1() {
+        assertEquals(true, NumberList.isIncreasing(Arrays.asList(1, 2, 3, 3, 3, 5)));
+        assertEquals(false, NumberList.isIncreasing(Arrays.asList(1, 2, 3, 2, 3, 5)));
+
+    }
+}

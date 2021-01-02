fca2278de788ed8a7ae785dccabd85ce8df4af44
n0rb1v <n0rb1v@gmail.com>
2021-01-02 21:50:12 +0100

    project

--- src/main/java/catalog/Catalog.java
+++ src/main/java/catalog/Catalog.java
@@ -11,30 +11,94 @@ public class Catalog {
     }
 
     public double averagePageNumberOver(int number) {
-        return 1;
+        if (number < 1) {
+            throw new IllegalArgumentException("Page number must be positive");
+        }
+        int result = 0;
+        int sum = 0;
+        for (CatalogItem item : catalogItems) {
+            if (item.hasPrintedFeature()) {
+                if (item.numberOfPagesAtOneItem() > number) {
+                    result += item.numberOfPagesAtOneItem();
+                    sum++;
+                }
+            }
+        }
+        if (sum == 0) {
+            throw new IllegalArgumentException("No page");
+        }
+        return result / (double) sum;
     }
 
     public void deleteItemByRegistrationNumber(String s) {
-
+        for (int i = 0; i < catalogItems.size(); i++) {
+            if (s.equals(catalogItems.get(i).getRegistrationNumber())) {
+                catalogItems.remove(i);
+            }
+        }
     }
 
     public int getAllPageNumber() {
-        return 1;
+        int result = 0;
+        for (CatalogItem item : catalogItems) {
+            if (item.hasPrintedFeature()) {
+                result += item.numberOfPagesAtOneItem();
+            }
+        }
+        return result;
     }
 
     public int getFullLength() {
-        return 1;
+        int result = 0;
+        for (CatalogItem item : catalogItems) {
+            if (item.hasAudioFeature()) {
+                result += item.fullLengthAtOneItem();
+            }
+        }
+        return result;
     }
 
-    public List<CatalogItem> findByCriteria(SearchCriteria searchCriteria){
-        return null;
+    public List<CatalogItem> findByCriteria(SearchCriteria searchCriteria) {
+        List<CatalogItem> result = new ArrayList<>();
+        for (CatalogItem item : catalogItems) {
+            if (searchCriteria.hasTitle() && searchCriteria.hasContributor()) {
+                if ((item.getTitles().toString().contains(searchCriteria.getTitle())) && (item.getContributors().toString().contains(searchCriteria.getContributor()))) {
+                    result.add(item);
+                }
+            }else {
+                if (searchCriteria.hasTitle()) {
+                    if (item.getTitles().toString().contains(searchCriteria.getTitle())) {
+                        result.add(item);
+                    }
+                }
+                if (searchCriteria.hasContributor()) {
+                    if (item.getContributors().toString().contains(searchCriteria.getContributor())) {
+                        result.add(item);
+                    }
+                }
+
+            }
+        }
+        return result;
     }
 
     public List<CatalogItem> getAudioLibraryItems() {
-        return null;
+        List<CatalogItem> result = new ArrayList<>();
+        for (CatalogItem item : catalogItems) {
+            if (item.hasAudioFeature()) {
+                result.add(item);
+            }
+        }
+        return result;
     }
 
     public List<CatalogItem> getPrintedLibraryItems() {
-        return null;
+        List<CatalogItem> result = new ArrayList<>();
+        for (CatalogItem item : catalogItems) {
+            if (item.hasPrintedFeature()) {
+                result.add(item);
+            }
+        }
+        return result;
     }
 }

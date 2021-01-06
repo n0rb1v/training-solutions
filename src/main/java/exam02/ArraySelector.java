cd83a5a8a6995fd2d3919dec239d869d0ec2a272
n0rb1v <n0rb1v@gmail.com>
2021-01-06 19:27:45 +0100

    vizsga end

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,5 +1,17 @@
 package exam02;
 
-public class ArraySelector {
+import java.util.ArrayList;
 
+public class ArraySelector {
+    public String selectEvens(int[] numbers) {
+        String result = "";
+        for (int i = 0; i < numbers.length; i += 2) {
+            result += numbers[i] + ", ";
+        }
+        if (result.isBlank()) {
+            return "";
+        }
+        result = result.substring(0, result.length() - 2);
+        return "[" + result + "]";
+    }
 }
--- src/main/java/exam02/cv/Cv.java
+++ src/main/java/exam02/cv/Cv.java
@@ -1,5 +1,40 @@
 package exam02.cv;
 
-public class Cv {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cv {
+    private String name;
+    private List<Skill> skills;
+
+    public Cv(String name) {
+        this.name = name;
+        this.skills = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void addSkills(String... s) {
+        for (String item : s) {
+            int lev = Integer.parseInt(item.substring(item.indexOf("(") + 1, item.indexOf("(") + 2));
+            String snam = item.substring(0, item.lastIndexOf(" "));
+            skills.add(new Skill(snam, lev));
+        }
+    }
+
+    public int findSkillLevelByName(String s) {
+        for (Skill item : skills) {
+            if (item.getName().equals(s)) {
+                return item.getLevel();
+            }
+        }
+        return 0;
+        //throw new SkillNotFoundException("Not found");
+    }
 }

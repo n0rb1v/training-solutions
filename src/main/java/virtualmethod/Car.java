16ff33adb7c6015873a4f3722ef1e63276380b1b
n0rb1v <n0rb1v@gmail.com>
2021-01-08 12:17:48 +0100

    virtualmethod

---
+++ src/main/java/virtualmethod/Car.java
@@ -0,0 +1,25 @@
+package virtualmethod;
+
+public class Car extends Vehicle {
+    private int numberPassenger;
+
+    public Car(int vehicleWeight, int numberPassenger) {
+        super(vehicleWeight);
+        this.numberPassenger = numberPassenger;
+    }
+
+    @Override
+    public int getGrossLoad() {
+        return super.getGrossLoad()+numberPassenger*PERSON_AVERAGE_WEIGHT;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{numberOfPassenger="+numberPassenger+", vehicleWeight="+getVehicleWeight()+"}" ;
+    }
+
+    public int getNumberPassenger() {
+        return numberPassenger;
+    }
+
+}

1c0faef74f4063da48d0032e443f41aef4ac7340
n0rb1v <n0rb1v@gmail.com>
2021-01-22 22:45:43 +0100

    w12d5

--- src/test/java/week12d05/IndexTest.java
+++ src/test/java/week12d05/IndexTest.java
@@ -2,10 +2,8 @@ package week12d05;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -15,9 +13,14 @@ class IndexTest {
     @Test
     void Test1() {
         InputStream is = Index.class.getResourceAsStream("/index.html");
+        String sample = "koronavírus\nalma\nkoronavírus\nkörte\nmeggy";
         try {
+            URL url = new URL("https://index.hu");
             assertEquals(9, new Index().countStr("koronavírus", new BufferedReader(new InputStreamReader(is))));
             assertEquals(0, new Index().countStr("koronavírus", Files.newBufferedReader(Path.of("unsecret.txt"))));
+            assertEquals(2, new Index().countStr("koronavírus", new BufferedReader(new StringReader(sample))));
+            assertEquals(9, new Index().countStr("koronavírus", new BufferedReader(new InputStreamReader(url.openStream()))));
+
         } catch (IOException e) {
             throw new IllegalStateException("hibas file", e);
         }

82fed44771be870e1ccd15d62634d886640cc0d5
n0rb1v <n0rb1v@gmail.com>
2021-02-02 19:02:30 +0100

    w14d1

--- src/main/java/week14d01/Indexer.java
+++ src/main/java/week14d01/Indexer.java
@@ -5,10 +5,10 @@ import java.util.*;
 public class Indexer {
     public Map<Character, List<String>> index(List<String> names) {
         Map<Character, List<String>> result = new HashMap<>();
-        for (String item: names){
+        for (String item : names) {
             char key = item.toUpperCase().charAt(0);
-            if (!result.containsKey(key)){
-                result.put(key,new ArrayList<>());
+            if (!result.containsKey(key)) {
+                result.put(key, new ArrayList<>());
             }
             result.get(key).add(item);
         }

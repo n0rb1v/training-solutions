dab1747b062c184fb576c3518774789749dd799c
n0rb1v <n0rb1v@gmail.com>
2021-02-11 19:16:44 +0100

    v2

--- src/main/java/lambdaintro/BankAccounts.java
+++ src/main/java/lambdaintro/BankAccounts.java
@@ -22,7 +22,7 @@ public class BankAccounts {
         result.sort(new Comparator<BankAccount>() {
             @Override
             public int compare(BankAccount o1, BankAccount o2) {
-                return (int) o1.getBalance() - (int) o2.getBalance();
+                return Math.abs((int)o1.getBalance()) - Math.abs((int) o2.getBalance());
             }
         });
         return result;
@@ -44,9 +44,34 @@ public class BankAccounts {
 
     public List<BankAccount> listBankAccountsByNameThanAccountNumber() {
         List<BankAccount> result = new ArrayList<>(bankAccounts);
-        Collator collator = Collator.getInstance(new Locale("hu","HU"));
-        Collections.sort(result,Comparator.comparing((x) -> x.getNameOfOwner(),Comparator.nullsFirst(collator)));
-
+        Collator c = Collator.getInstance(new Locale("hu", "HU"));
+//        Collections.sort(result, new Comparator<BankAccount>() {
+//            @Override
+//            public int compare(BankAccount o1, BankAccount o2) {
+//                if (o1.getNameOfOwner() == null) {
+//                    return -1;
+//                }
+//                if (o2.getNameOfOwner() == null) {
+//                    return 1;
+//                }
+//                if (!o1.getNameOfOwner().equals(o2.getNameOfOwner())) {
+//                    return o1.getNameOfOwner().compareTo(o2.getNameOfOwner());
+//                }else {
+//                    return o1.getAccountNumber().compareTo(o2.getAccountNumber());
+//                }
+//            }
+//        });
+        Collections.sort(result, Comparator.nullsFirst(new Comparator<BankAccount>() {
+            @Override
+            public int compare(BankAccount o1, BankAccount o2) {
+                return o1.getNameOfOwner().compareTo(o2.getNameOfOwner());
+            }
+        }.thenComparing(new Comparator<BankAccount>() {
+            @Override
+            public int compare(BankAccount o1, BankAccount o2) {
+                return o1.getAccountNumber().compareTo(o2.getAccountNumber());
+            }
+        })));
         return result;
 
     }

21ea30a2b89b86fe4bf3a1bbcdfff0368b5307b0
n0rb1v <n0rb1v@gmail.com>
2021-02-15 18:15:25 +0100

    exam03 #4

--- src/main/java/exam03/Cruise.java
+++ src/main/java/exam03/Cruise.java
@@ -1,4 +1,80 @@
 package exam03;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 public class Cruise {
+    private Boat boat;
+    private LocalDate sailing;
+    private double basicPrice;
+    private List<Passenger> passengers = new ArrayList<>();
+
+    public Cruise(Boat boat, LocalDate sailing, double basicPrice) {
+        this.boat = boat;
+        this.sailing = sailing;
+        this.basicPrice = basicPrice;
+    }
+
+    public Boat getBoat() {
+        return boat;
+    }
+
+    public LocalDate getSailing() {
+        return sailing;
+    }
+
+    public double getBasicPrice() {
+        return basicPrice;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void bookPassenger(Passenger passenger) {
+        if (getPassengers().size() < boat.getMaxPassengers()) {
+            passengers.add(passenger);
+        } else {
+            throw new IllegalArgumentException("param error");
+        }
+    }
+
+    public double getPriceForPassenger(Passenger passenger) {
+        return basicPrice * passenger.getCruiseClass().getValue();
+
+    }
+
+    public Passenger findPassengerByName(String s) {
+        Passenger result = null;
+        for (Passenger item : passengers) {
+            if (item.getName().equals(s)) {
+                result = item;
+            }
+        }
+        return result;
+    }
+
+    public List<String> getPassengerNamesOrdered() {
+        List<String> result = new ArrayList<>();
+        for (Passenger item: passengers){
+            result.add(item.getName());
+        }
+        Collections.sort(result);
+        return result;
+    }
+
+    public double sumAllBookingsCharged() {
+        double result = 0;
+        for (Passenger item: passengers){
+            result += item.getCruiseClass().getValue()*basicPrice;
+        }
+        return result;
+    }
+
+    public Map<CruiseClass, Integer> countPassengerByClass() {
+        return null;
+    }
 }

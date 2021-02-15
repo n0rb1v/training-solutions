33169b1c532a08998cdbf97a0841c4f5c5a432e1
n0rb1v <n0rb1v@gmail.com>
2021-02-15 17:28:53 +0100

    exam03 #2

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,4 +1,19 @@
 package exam03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Histogram {
+
+    public String createHistogram(BufferedReader reader) throws IOException {
+        String line;
+        String result = null;
+        while ((line= reader.readLine())!=null){
+            for (int i = 0; i < Integer.parseInt(line); i++) {
+                result.concat("*");
+            }
+            result.concat("\n");
+        }
+        return result;
+    }
 }
